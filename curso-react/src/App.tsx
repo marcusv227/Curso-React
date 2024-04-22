6ca5c93022dc8f4669f28f7a31ecbf48fbeb15c4
@marcus.silva <marcus.silva@iatecam.org.br>
2024-04-22 16:28:58 -0400

    add: new components and API creation

--- curso-react/src/App.tsx
+++ curso-react/src/App.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import './App.css'
+import Create from './components/create';
 
 function App() {
   return (
-    <div className="App">
-      <TextField id="outlined-basic" label="E-mail" variant="outlined" /> 
-      <TextField id="outlined-basic" label="Senha" variant="outlined" />
-      <Button variant="contained">Cadastrar</Button>
+    <div className='app'>
+      <Create/>
     </div>
   );
 }
